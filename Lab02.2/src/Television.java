946a57a5a5d0e2f8ffc360689a5dd5893870be0a
yaping0816 <yaping0816@gmail.com>
2022-09-06 15:05:22 -0700

    Completed Lab3.4.

--- Lab02.2/src/Television.java
+++ Lab02.2/src/Television.java
@@ -1,7 +1,26 @@
 public class Television {
 
+  public static final int MIN_VOLUME = 0;
+  public static final int MAX_VOLUME = 100;
+
+  private static int instanceCount = 0;
+
   private  String brand;
   private int volume;
+
+  public Television(){
+    instanceCount++;
+  }
+
+  public Television(String brand) {
+    this();
+    setBrand(brand);
+  }
+
+  public Television(String brand, int volume) {
+    this(brand);
+    setVolume(volume);
+  }
 
   public void turnOn(){
     boolean isConnected = verifyInternetConnection();
@@ -10,6 +29,10 @@ public class Television {
 
   public void turnOff(){
     System.out.println("The " + brand + " television is off");
+  }
+
+  public static int getInstanceCount() {
+    return instanceCount;
   }
 
   public String getBrand() {
@@ -25,7 +48,13 @@ public class Television {
   }
 
   public void setVolume(int volume) {
-    this.volume = volume;
+    if (volume < MIN_VOLUME || volume >MAX_VOLUME){
+      //%d is a placeholder for base 10 representation of integer value, here volume, MIN_VOLUME and MAX_VOLUME; $n for new line
+      System.out.printf("%d is invalid; volume muse between %d and %d (inclusive).%n", volume, MIN_VOLUME, MAX_VOLUME);
+
+    }else {
+      this.volume = volume;
+    }
   }
 
   private boolean verifyInternetConnection() {
--- Lab02.2/src/TelevisionClient.java
+++ Lab02.2/src/TelevisionClient.java
@@ -3,20 +3,38 @@ public class TelevisionClient {
   public static void main(String[] args) {
     Television tv1 = new Television();
     tv1.setBrand("Samsung");
-    tv1.setVolume(25);
-
-    Television tv2 = new Television();
-     tv2.setBrand("Lenovo");
-     tv2.setVolume(30);
+    tv1.setVolume(125);
 
      tv1.turnOn();
      tv1.turnOff();
-
-     tv2.turnOn();
-     tv2.turnOff();
-
     System.out.println(tv1.toString());
     System.out.println(tv1);
+
+    System.out.printf("%d television instances created%n",Television.getInstanceCount());
+
+    Television tv2 = new Television("Lenovo", 30);
+//     tv2.setBrand("Lenovo");
+//     tv2.setVolume(30);
+     tv2.turnOn();
+     tv2.turnOff();
+    System.out.println(tv2);
+    System.out.printf("%d television instances created%n",Television.getInstanceCount());
+
+
+//     tv3.turnOn();
+//     tv3.turnOff();
+    Television tv3 = new Television("LG");
+    System.out.println(tv3);
+//    System.out.println(new Television("LG")); //same as line 26+27
+    System.out.printf("%d television instances created%n",Television.getInstanceCount());
+
+
+
+
+
+
+
+
 
   }
 
