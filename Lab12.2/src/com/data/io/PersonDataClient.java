17fc8269ab6c97e1ce36b51248bcdd3b4bc1831f
yaping0816 <yaping0816@gmail.com>
2022-09-19 13:57:09 -0700

    Completed Lab12.2

--- Lab12.2/src/com/data/io/PersonDataClient.java
+++ Lab12.2/src/com/data/io/PersonDataClient.java
@@ -8,14 +8,21 @@
 
 package com.data.io;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class PersonDataClient {
 
     /**
      * To run one method at a time, uncomment the call to the one you want to execute.
      */
     public static void main(String[] args) {
-        // writeData();
-        // readData();
+         writeData();
+         readData();
     }
 
     /**
@@ -30,6 +37,15 @@ public class PersonDataClient {
      */
     private static void writeData() {
         // TODO
+        try (DataOutputStream out = new DataOutputStream(new FileOutputStream("person.dat"))){
+            out.writeUTF("Yaping Liu");
+            out.writeInt(28);
+            out.writeDouble(6.5);
+            out.writeBoolean(true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
     /**
@@ -43,5 +59,21 @@ public class PersonDataClient {
      */
     private static void readData() {
         // TODO
+        try(DataInputStream in = new DataInputStream(new FileInputStream("person.dat"))) {
+//            String name = in.readUTF();
+//            int age = in.readInt();
+//            double shoeSize = in.readDouble();
+//            boolean married = in.readBoolean();
+//            System.out.println("Name: "+ name + ", age: "+ age + "shoe size is: "+ shoeSize+ ", married?: "+ married);
+            while(in.available() != 0){
+                System.out.println(in.readUTF());
+                System.out.println(in.readInt());
+                System.out.println(in.readDouble());
+                System.out.println(in.readBoolean());
+//                System.out.println(in.readByte());
+            }
+        } catch(IOException e){
+            e.printStackTrace();
+        }
     }
 }
